a0b8aff79f7db5dd934a5cfa96377e401a63e220
IgorChvyrov-sm <igor.chvyrov@siliconmint.com>
2025-07-15 17:39:11 +0300

    Generated conductor client for v3.21.16

---
+++ src/conductor/client/conductor_v3/api/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from conductor_v3.api.admin_resource_api import AdminResourceApi
+from conductor_v3.api.event_resource_api import EventResourceApi
+from conductor_v3.api.health_check_resource_api import HealthCheckResourceApi
+from conductor_v3.api.metadata_resource_api import MetadataResourceApi
+from conductor_v3.api.queue_admin_resource_api import QueueAdminResourceApi
+from conductor_v3.api.task_resource_api import TaskResourceApi
+from conductor_v3.api.workflow_bulk_resource_api import WorkflowBulkResourceApi
+from conductor_v3.api.workflow_resource_api import WorkflowResourceApi
